84aa711f0d918deef173ee86b5d21396802cd58f
Erick Ruiz <EERG05@hotmail.com>
2021-10-06 00:38:13 -0500

    Files for the web page and firebaseJSON configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../public/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Menu Alexander</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
